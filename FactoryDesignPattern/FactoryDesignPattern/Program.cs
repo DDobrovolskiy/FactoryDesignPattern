b762a4af7afe1126fbfbb75641c50fd60047f17a
DmitriyDobrovolskiy <dda.90@mail.ru>
2020-09-24 22:32:03 +0500

    Create pattern

---
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Design Pattern!");
+            Console.WriteLine("---");
+            Console.ReadKey();
+        }
+    }
+}
